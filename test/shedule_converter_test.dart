49ac294fe2009f2ad61fda5527747d4645c6f5f8
wupidi <avazshokiri@gmail.com>
2022-03-24 23:03:48 +0300

    fix test

--- test/shedule_converter_test.dart
+++ test/shedule_converter_test.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:excel/excel.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:schedule_mirea/utils/shedule_converter.dart';
+import 'package:schedule_mirea/utils/schedule_converter.dart';
 
 void main() {
   group('', (){
@@ -10,7 +10,7 @@ void main() {
       // это очень тупой тест, но потыкаться проверить можно
       // можно запустить debug поставив точку останова в строке print
       const file = 'assets/ИИТ_1 курс_21-22_весна.xlsx';
-      final converter = SheduleConverter();
+      final converter = ScheduleConverter();
       converter.setFile(file);
       final subjects = converter.getSubjectsOnWeek('ИВБО-13-21'); 
       print(subjects);
@@ -18,7 +18,7 @@ void main() {
 
     test('get all subjects', (){
       const file = 'assets/ИИТ_1 курс_21-22_весна.xlsx';
-      final converter = SheduleConverter();
+      final converter = ScheduleConverter();
       converter.setFile(file);
       final subjects = converter.getAllSubjects('ИВБО-13-21');
       print(subjects);
