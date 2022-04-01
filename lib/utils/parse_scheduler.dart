1ad0f79259735d48e60e1877b82ca822a46630b2
Pod1um76 <lynant77@gmail.com>
2022-04-01 14:10:21 +0300

    add list with forbidden words, rename  "_receivedCode" methods, adjust and rename "_enteredCode" method's

--- lib/utils/parse_scheduler.dart
+++ lib/utils/parse_scheduler.dart
@@ -4,6 +4,7 @@ import 'package:html/parser.dart' as html;
 
 class ParseScheduler {
   late String _group;
+
   final Map<String, String> _universGroups = {
     'ИПТИП': 'Э',
     'ИТУ': 'У',
@@ -12,6 +13,8 @@ class ParseScheduler {
     'ИКБ': 'Б',
     'ИРЭИ': 'Р'
   };
+
+  final _forbiddenWords = <String>{"Стромынка", "экз"};
 
   ParseScheduler(String group) {
     _group = group;
@@ -25,31 +28,30 @@ class ParseScheduler {
     return _group;
   }
 
-  String _enteredCode() {
+  String _enteredCodeGroups() {
     int groupLenght = _group.length;
 
     return (_group[0] + _group.substring(groupLenght - 2, groupLenght));
   }
 
-  String _receivedCode(String code) {
+  String? _receivedCodeForScheduler(String fileName) {
     const String marker = "экз";
     final nowYear = DateTime.now().year;
 
-    if (code.split('_')[1].length > 1) {
-      code = code.replaceAll('_Стромынка', '');
-    } else if (code.split('_')[0] != marker) {
-      //This is temporary crutch
-      String university = code.split('_')[0];
-
-      int yearAdmission = nowYear - int.parse(code.split('_')[1]);
-      String year = yearAdmission.toString();
-      int yearSize = year.length;
-
-      return (_universGroups[university]! +
-          year.substring(yearSize - 2, yearSize));
+    for (String word in _forbiddenWords) {
+      if (fileName.contains(word)) {
+        return null;
+      }
     }
 
-    return marker;
+    String university = fileName.split('_')[0];
+
+    int yearAdmission = nowYear - int.parse(fileName.split('_')[1]);
+    String year = yearAdmission.toString();
+    int yearSize = year.length;
+
+    return (_universGroups[university]! +
+        year.substring(yearSize - 2, yearSize));
   }
 
   Future<String?> getLink() async {
@@ -73,7 +75,7 @@ class ParseScheduler {
         String nameLink = link.replaceAll(' ', '_').split('/').last;
         //print(_receivedCode(nameLink));
 
-        if (_enteredCode() == _receivedCode(nameLink)) {
+        if (_enteredCodeGroups() == _receivedCodeForScheduler(nameLink)) {
           return link;
         }
 
