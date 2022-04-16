dd0a0fe728d707ed156298a31974275e09ab5d49
wupidi <avazshokiri@gmail.com>
2022-04-16 14:32:06 +0300

    create notification_controller

---
+++ lib/controllers/notihfication_controller.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'tasks_controller.dart';
+import '../utils/settings.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationController {
+  final TasksController _tasksController;
+  final Settings _settings;
+  final _notification = FlutterLocalNotificationsPlugin();
+  final _onNotifications = StreamController<String?>();
+  Stream<String?> onNotifications() => _onNotifications.stream;
+
+  NotificationController(this._tasksController, this._settings);
+
+  Future<void> init() async {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const iOS = IOSInitializationSettings();
+    const settings = InitializationSettings(android: android, iOS: iOS);
+    await _notification.initialize(settings,
+        onSelectNotification: (payload) async {
+      _onNotifications.add(payload);
+    });
+    tz.initializeTimeZones();
+  }
+
+  Future<void> updateNotifications([String? groupCode]) async {
+    final code = groupCode ?? await _settings.getGroup();
+    if (code == null) {
+      throw Exception("Grope code not exist in the settings");
+    }
+
+    await _notification.cancelAll();
+
+    final tasks = await _tasksController.getTasks(groupCode: code);
+    final notificationDays = (await _settings.getDays());
+
+    tasks
+        .where((element) => DateTime.now().compareTo(element.deadline) == -1)
+        .forEach((element) async {
+      late final DateTime time;
+      late final String title;
+      late final String body;
+      final subject = await _tasksController.getSubjectByTask(element.id);
+      if (element.deadline.day - DateTime.now().day >= notificationDays) {
+        time = element.deadline.add(Duration(days: -notificationDays));
+        title = "Приближается дедлайн по задаче ${element.name}";
+        body = "До дедлайна дней: $notificationDays \n"
+            "Задача: ${element.name}\n"
+            "Предмет: ${subject.name}";
+      } else {
+        time = element.deadline.add(const Duration(days: -1));
+        title = "Завтра дедлайн по задаче ${element.name}";
+        body = "До дедлайна дней: 1 \n"
+            "Задача: ${element.name}\n"
+            "Предмет: ${subject.name}";
+      }
+
+      _scheduleNotification(
+          id: element.id, title: title, body: body, time: time);
+    });
+  }
+
+  Future<void> _scheduleNotification({
+    int id = 0,
+    String? title,
+    String? body,
+    String? payload,
+    required DateTime time,
+  }) async {
+    _notification.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(time, tz.local),
+      await _notificationDetails(),
+      payload: payload,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      matchDateTimeComponents: DateTimeComponents.dateAndTime,
+    );
+  }
+
+  Future<NotificationDetails> _notificationDetails() async {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'channel id',
+        'channel name',
+        channelDescription: 'channel description',
+        importance: Importance.max,
+      ),
+      iOS: IOSNotificationDetails(),
+    );
+  }
+}
+
+final notificationControllerProvider = Provider((ref) => NotificationController(
+    ref.watch(tasksControllerProvider), ref.watch(settingsProvider)));
