3c59dd494470bf1a4b1b4ffaf248519612663d8f
wupidi <avazshokiri@gmail.com>
2022-04-17 21:14:16 +0300

    fix example in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int? subjectId;
   final List<Task> _tasks = [];
-  final controller = ScrollController();
 
   @override
   void initState() {
@@ -42,7 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> init() async {
     await MethodsProvider.get().db.init();
     await MethodsProvider.get().settings.init();
-    MethodsProvider.get().settings.setGroup('ИВБО-02-19');
+    MethodsProvider.get().settings
+      ..setGroup('ИВБО-02-19')
+      ..setTimeNotification(const TimeOfDay(hour: 12, minute: 0));
     final subjects = await MethodsProvider.get()
         .scheduleController
         .getSubjects('ИВБО-02-19');
@@ -63,6 +64,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final children = _tasks
+        .map(
+          (e) => ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TaskEditorPage(
+                    subjectId: subjectId!,
+                    task: e,
+                  ),
+                ),
+              );
+            },
+            child: Text(e.name),
+          ),
+        )
+        .toList();
     return Scaffold(
       appBar: AppBar(),
       body: Container(
@@ -71,34 +90,35 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text('Loading ...'),
               )
             : RefreshIndicator(
-              onRefresh: () async {
-                final tasks = await MethodsProvider.get().tasksController.getTasks(groupCode: 'ИВБО-02-19', subjectId: subjectId);
-                setState(() {
-                  _tasks.clear();
-                  _tasks.addAll(tasks);
-                });
-              },
-              child: ListView.builder(
+                onRefresh: () async {
+                  final tasks = await MethodsProvider.get()
+                      .tasksController
+                      .getTasks(groupCode: 'ИВБО-02-19', subjectId: subjectId);
+                  setState(() {
+                    _tasks.clear();
+                    _tasks.addAll(tasks);
+                  });
+                },
+                child: ListView(
                   physics: const AlwaysScrollableScrollPhysics(),
-                  controller: controller,
-                  itemCount: _tasks.length,
-
-                  itemBuilder: (_, index) => ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TaskEditorPage(
-                            subjectId: subjectId!,
-                            task: _tasks[index],
+                  children: [
+                    ...children,
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TaskEditorPage(
+                              subjectId: subjectId!,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                    child: Text(_tasks[index].name),
-                  ),
+                        );
+                      },
+                      child: Icon(Icons.add),
+                    ),
+                  ],
                 ),
-            ),
+              ),
       ),
     );
   }
