0708145dbd553931d32cbc9e7cb28e2f54e5bf33
wupidi <avazshokiri@gmail.com>
2022-04-21 22:53:24 +0300

    create settings_page.dart

---
+++ lib/ui/settings_page/group_settings_field.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:schedule_mirea/ui/settings_page/settings_page_state.dart';
+
+import '../../methods_provider.dart';
+import '../consts.dart';
+
+class GroupSettingsField extends StatefulWidget {
+  final SettingsPageState state;
+  const GroupSettingsField({Key? key, required this.state}) : super(key: key);
+
+  @override
+  State<GroupSettingsField> createState() => _GroupSettingsFieldState();
+}
+
+class _GroupSettingsFieldState extends State<GroupSettingsField> {
+  SettingsPageState get state => widget.state;
+  late final TextEditingController groupFieldController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    groupFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                widget.state.selectedGroupCode == null
+                    ? 'Введи наименование группы:'
+                    : 'Выбрано наименование группы:',
+                style: const TextStyle(
+                  color: kTextColor,
+                  fontSize: 10,
+                ),
+              ),
+              const SizedBox(width: 1),
+              Text(
+                widget.state.selectedGroupCode ?? '',
+                style: const TextStyle(
+                  color: kAccentColor,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+          if (state.groupCodeChangeMode)
+            TextFormField(
+              controller: groupFieldController,
+              style: const TextStyle(
+                fontSize: 16,
+                color: kTextColor,
+              ),
+              inputFormatters: [Formatter(),],
+              decoration: InputDecoration(
+                errorText: state.groupCodeError,
+                hintText: 'Например: ИВБО-02-19',
+                hintStyle: const TextStyle(
+                  color: kHintTextColor,
+                ),
+                filled: true,
+                fillColor: kSecondaryColor,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: kAccentColor,
+                fixedSize: const Size(110, 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                )),
+            onPressed: () {
+              if (state.groupCodeChangeMode) {
+                final groupCode = groupFieldController.value.text;
+                MethodsProvider.get()
+                    .settingsPageController
+                    .updateGroupCode(groupCode.toUpperCase());
+              } else {
+                MethodsProvider.get()
+                    .settingsPageController
+                    .turnOnChangeModeGroupCode();
+              }
+            },
+            child: Text(
+              state.groupCodeChangeMode ? 'Загрузить расписание' : 'Изменить',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            height: 100,
+            color: kSecondaryColor,
+            child: ListView(
+              children: state.loadedGroupCode
+                  .map(
+                    (e) => TextButton(
+                      onPressed: () {
+                        MethodsProvider.get()
+                            .settingsPageController
+                            .updateGroupCode(e);
+                      },
+                      child: Text(
+                        e,
+                        style: const TextStyle(color: kAccentColor),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Formatter extends TextInputFormatter {
+  final groupMask = '0000-00-00';
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    final result = StringBuffer();
+    var text =
+    newValue.text.replaceAll('-', '').replaceAll(' ', '').toUpperCase();
+    if (text.length > 1 && oldValue.text.length > newValue.text.length) {
+      text = text.substring(0, text.length);
+    }
+    var readPosition = 0;
+    for (var i = 0; i < groupMask.length; i++) {
+      if (readPosition > text.length - 1) {
+        break;
+      }
+      var curSymbol = groupMask[i];
+      if (isZeroSymbol(curSymbol)) {
+        curSymbol = text[readPosition];
+        readPosition++;
+      }
+      result.write(curSymbol);
+    }
+    final textResult = result.toString();
+    return TextEditingValue(
+      text: textResult,
+      selection: TextSelection.collapsed(
+        offset: textResult.length,
+      ),
+    );
+  }
+
+  bool isZeroSymbol(String symbol) => symbol == "0";
+}
--- lib/ui/settings_page/settings_page.dart
+++ lib/ui/settings_page/settings_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:schedule_mirea/methods_provider.dart';
+import 'package:schedule_mirea/ui/settings_page/group_settings_field.dart';
+
+import '../consts.dart';
+import 'settings_page_state.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -8,10 +13,48 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    MethodsProvider.get().settingsPageController.init();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('SETTINGS PAGE'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Параметры',
+            style: TextStyle(
+              color: kTextColor,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        titleSpacing: 0,
+        toolbarHeight: 100,
+        elevation: 0,
+      ),
+      body: StreamBuilder<SettingsPageState?>(
+        stream: MethodsProvider.get().settingsPageState.stream,
+        builder: (context, snapshot) {
+          if (snapshot.data == null || snapshot.data!.loaded) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+              children: [GroupSettingsField(state: snapshot.data!)]);
+        },
+      ),
     );
   }
 }
