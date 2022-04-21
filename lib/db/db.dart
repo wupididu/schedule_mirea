c637770440c70583b502f97daf01b941bec9d5e8
wupidi <avazshokiri@gmail.com>
2022-04-21 22:48:55 +0300

    add method deleteUselessSchedule for db

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -79,6 +79,7 @@ class DB {
 
     await _deleteUselessSubject();
     await _deleteUselessTask();
+    await deleteUselessSchedule(groupCode);
   }
 
   Future<List<DBGroups>> getGroups() async {
@@ -222,7 +223,6 @@ class DB {
     return resTask.map((e) => DBTask.fromMap(e)).toList();
   }
 
-
   Future<void> deleteTask(int taskId) async {
     final removedTaskId = await _db.delete(
       DBTask.tableName,
@@ -258,7 +258,7 @@ class DB {
       s.${DBSubject.columnId} = g.${DBGroupXTask.columnSubjectId}
     ''';
     final result = await _db.rawQuery(query);
-    if (result.isEmpty){
+    if (result.isEmpty) {
       throw Exception('task not have subject');
     }
     return DBSubject.fromMap(result.first);
@@ -384,6 +384,12 @@ class DB {
   Future<List<DBTask>> _getTasks() async {
     final resTask = await _db.query(DBTask.tableName);
     return resTask.map((e) => DBTask.fromMap(e)).toList();
+  }
+
+  Future<void> deleteUselessSchedule(String groupCode) async {
+    final groupId = await _findGroupId(groupCode);
+    await _db.delete(DBSchedule.tableName,
+        where: '${DBSchedule.columnGroupId} = ?', whereArgs: [groupId]);
   }
 }
 
