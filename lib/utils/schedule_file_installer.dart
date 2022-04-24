5334982b99ffe5a5666fe5a8dd22db5819d1a159
wupidi <avazshokiri@gmail.com>
2022-04-24 15:30:39 +0300

    add doc for schedule_file_installer.dart

--- lib/utils/schedule_file_installer.dart
+++ lib/utils/schedule_file_installer.dart
@@ -4,12 +4,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' show get;
 import 'package:path_provider/path_provider.dart';
 
+/// Данная утилита устанавливает файл с расписанием в определенное место
+/// на устройстве из сети по URL адресу
 class ScheduleFileInstaller {
+
+  /// Метод возвращает место, где лежит файл
   Future<String> get scheduleFilePath async {
     var documentDirectory = await getApplicationDocumentsDirectory();
     return documentDirectory.path + "/schedule.xlsx";
   }
 
+  /// Данный метод устанваливает файл из uri
   Future<void> downloadFile(String uri) async {
     var url = Uri.tryParse(uri);
     if (url == null) {
