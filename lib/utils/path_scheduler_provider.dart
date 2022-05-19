39d7372038f48900de44c35898c669e1a11bef6f
wupidi <avazshokiri@gmail.com>
2022-05-19 23:09:06 +0300

    refactoring parse urls for files

--- lib/utils/path_scheduler_provider.dart
+++ lib/utils/path_scheduler_provider.dart
@@ -7,75 +7,100 @@ import 'package:html/parser.dart' as html;
 /// Это утилита, которая предоставляет возможность получить ссылку на файл,
 /// от кода группы.
 class PathSchedulerProvider {
-  late String _group;
-
-  final Map<String, String> _universGroups = {
-    'иптип': 'Э',
-    'иту': 'У',
-    'иит': 'И',
-    'иии': 'К',
-    'икб': 'Б',
-    'ирэи': 'Р'
+  final Map<String, List<String>> _universGroups = {
+    'иптип': ['ЭО', 'ЭЛ', 'ЭН', 'ЭЭ', 'ЭС'],
+    'иптип_стр': ['ТШ', 'ТД', 'ТХ', 'ТК', 'ТЛ'],
+    'иту': ['УД', 'УИ', 'УС'],
+    'иту_стр': ['УП', 'УП', 'УУ', 'УН', 'УЮ', 'УМ'],
+    'иит': ['ИА', 'ИВ', 'ИК', 'ИМ', 'ИН'],
+    'иии': ['КB', 'КА', 'КМ', 'КБ', 'КС', 'КР', 'КТ', 'КК'],
+    'икб': ['БА', 'ББ', 'БИ', 'БС', 'БА', 'БП', 'БФ', 'БЭ', 'БО'],
+    'ирэи': ['РС', 'РР', 'РИ', 'РК'],
+    'итхт': ['ХЕ', 'ХТ', 'ХБ', 'ХХ']
   };
 
   final _forbiddenWords = <String>{
-    'стромынка',
+    'docx',
+    'pdf',
+    'rtf',
     'экз',
     'маг',
     'расписание',
-    'колледж'
+    'колледж',
   };
 
-  String _enteredCodeGroups() {
-    int groupLenght = _group.length;
+  String _enteredCodeGroups(String groupCode) {
+    int groupLenght = groupCode.length;
 
-    return (_group[0] + _group.substring(groupLenght - 2, groupLenght));
+    return (groupCode.substring(0, 2) + groupCode.substring(groupLenght - 2));
   }
 
-  String? _receivedCodeForScheduler(String fileName) {
+  List<String> _receivedCodesForScheduler(String fileName) {
     final nowYear = DateTime.now().year;
     fileName = fileName.toLowerCase();
 
-    for (String word in _forbiddenWords) {
-      if (fileName.contains(word)) {
-        return null;
-      }
+    final university = fileName.split('_')[0];
+
+    late final List<String>? supportsGroups;
+
+    if (university == 'иптип' && fileName.contains('сем')) {
+      supportsGroups = _universGroups['иптип_стр'];
+    } else if (university == 'иту' && fileName.contains('сем')) {
+      supportsGroups = _universGroups['иту_стр'];
+    } else {
+      supportsGroups = _universGroups[university];
     }
 
-    String university = fileName.split('_')[0];
+    late final String codeYear;
+    final elements = fileName.split('_');
+    if (fileName.contains('курс')) {
+      final index = elements.indexOf('курс');
+      final yearAdmission = int.parse(elements[index-1]);
+      codeYear = (nowYear - yearAdmission).toString().substring(2);
+    } else if (fileName.contains(RegExp(r'[0-9]к'))) {
+      final element = elements.firstWhere((element) => RegExp(r'[0-9]к').hasMatch(element))[0];
+      final yearAdmission = int.parse(element);
+      codeYear = (nowYear - yearAdmission).toString().substring(2);
+    } else {
+      codeYear = '';
+    }
 
-    int yearAdmission = nowYear - int.parse(fileName.split('_')[1]);
-    String year = yearAdmission.toString();
-    int yearSize = year.length;
 
-    return (_universGroups[university]! +
-        year.substring(yearSize - 2, yearSize));
+    return supportsGroups?.map((e) => e + codeYear).toList() ?? [];
   }
 
   /// Данный метда предоставляет ссылку на файл в зависимости от [groupCode]
   ///
   /// Если ссылка на файл не найдена, то возварщается [null]
   Future<String?> getLink(String groupCode) async {
-    _group = groupCode;
     final response =
         await http.Client().get(Uri.parse("https://www.mirea.ru/schedule/"));
 
     Document document = await compute(html.parse, response.body);
 
-    return await compute(_getLink,document);
+    return await compute(
+        _getLink, {'document': document, 'group_code': groupCode});
   }
 
-  String? _getLink(Document document) {
+  String? _getLink(Map<String, dynamic> params) {
+    final document = params['document'] as Document;
+    final groupCode = params['group_code'] as String;
+
     final anchors = document.querySelectorAll('a');
+    final neededAnchors = anchors
+        .where((element) => element.attributes['class'] == 'uk-link-toggle');
+    final links = neededAnchors.map((e) => e.attributes['href']);
+    final enteredCodeGroup = _enteredCodeGroups(groupCode);
 
-    for (var anchor in anchors) {
-      if (anchor.attributes['class'] == 'uk-link-toggle') {
-        var href = anchor.attributes['href'];
-        String link = href.toString();
+    for (var link in links) {
+      String nameLink = link?.replaceAll(' ', '_').split('/').last ?? '';
 
-        String nameLink = link.replaceAll(' ', '_').split('/').last;
+      if (_forbiddenWords
+          .where((element) => nameLink.contains(element))
+          .isEmpty) {
+        List<String> codeGroupsForFile = _receivedCodesForScheduler(nameLink);
 
-        if (_enteredCodeGroups() == _receivedCodeForScheduler(nameLink)) {
+        if (codeGroupsForFile.contains(enteredCodeGroup)) {
           return link;
         }
       }
